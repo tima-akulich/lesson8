12cd28daba137dc3cbe453dbd040a7b21ee5cc95
lucecita1987 <luzzambrano277@gmail.com>
2018-10-04 03:01:20 +0300

    fraction_homework.py

---
+++ test_fraction.py
@@ -0,0 +1,17 @@
+from fraction_homework import Fraction
+
+
+
+
+
+x=Fraction(3,1)
+if(str(x)=="3/1"):
+    print('Pass')
+else:
+    print('Fail')
+
+x = Fraction(2, 1)
+if (str(x) == "3/1"):
+    print('Pass')
+else:
+    print('Fail')
---
+++ test_fraction2.py
@@ -0,0 +1,45 @@
+from fraction_homework import Fraction
+
+
+a=Fraction(2,4)
+b=Fraction(-1,4)
+c=Fraction(3,3)
+e=Fraction(2,8)
+x=Fraction(2,4)
+y=Fraction(1,4)
+z=Fraction(5,4)
+
+if (x==a):
+    print("Pass")
+else:
+    print("Fail")
+if (x !=z):
+    print("Pass")
+else:
+    print("Fail")
+
+if(y!=e):
+    print("Pass")
+else:
+    print("Fail")
+
+if (z < y):
+    print("Pass")
+else:
+    print("Fail")
+
+if (x > e):
+    print("Pass")
+else:
+    print("Fail")
+
+if (c>=e):
+    print("Pass")
+else:
+    print("Fail")
+
+if(e<=c):
+    print("Pass")
+else:
+    print("Fail")
+
